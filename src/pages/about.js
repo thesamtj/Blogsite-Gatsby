fff00c6b20a29c8601c0cffc3969cbdf675afd9f
Samuel Tijani <omalitijani@gmail.com>
2020-05-02 14:53:35 +0100

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        lorem lkjdfgjk lkjdggldk kjgf ijgeorig ijofigjkr kjgjfjk gjkflj
+        rgjkfelfj erjgklefj oergjrk erjgkffoi kfjlsj ioefikj ijkfeflj jkfljf
+        jkfejf eirujkelfj ekhjfkflsjd ijkeslfj jkkfla kj ogfuigfjklf erj eofijk
+        eirgjkg eirgkj rigjkleikjl orijkls jk igjkls srikjse kjrglrs kjld gkjgdl
+        slkll sjklts slkjglf slkls slk slk slkgl slkfg lgkf sklgk lkg;lkd jklgds
+        klgjkfld kgll slklskgl flkjgl;s slkjsglk sklklg sklgjl slkjlg lksjg
+        slkfj{" "}
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
