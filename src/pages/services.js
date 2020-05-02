6d679e09490fd3587a23caa85f0ea35dc33c2e4a
Samuel Tijani <omalitijani@gmail.com>
2020-05-02 15:02:47 +0100

    Add services page

---
+++ src/pages/services.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>
+        lorem lkjdfgjk lkjdggldk kjgf ijgeorig ijofigjkr kjgjfjk gjkflj
+        rgjkfelfj erjgklefj oergjrk erjgkffoi kfjlsj ioefikj ijkfeflj jkfljf
+        jkfejf eirujkelfj ekhjfkflsjd ijkeslfj jkkfla kj ogfuigfjklf erj eofijk
+        eirgjkg eirgkj rigjkleikjl orijkls jk igjkls srikjse kjrglrs kjld gkjgdl
+        slkll sjklts slkjglf slkls slk slk slkgl slkfg lgkf sklgk lkg;lkd jklgds
+        klgjkfld kgll slklskgl flkjgl;s slkjsglk sklklg sklgjl slkjlg lksjg
+        slkfj{" "}
+      </p>
+      <p>
+        lorem lkjdfgjk lkjdggldk kjgf ijgeorig ijofigjkr kjgjfjk gjkflj
+        rgjkfelfj erjgklefj oergjrk erjgkffoi kfjlsj ioefikj ijkfeflj jkfljf
+        jkfejf eirujkelfj ekhjfkflsjd ijkeslfj jkkfla kj ogfuigfjklf erj eofijk
+        eirgjkg eirgkj rigjkleikjl orijkls jk igjkls srikjse kjrglrs kjld gkjgdl
+        slkll sjklts slkjglf slkls slk slk slkgl slkfg lgkf sklgk lkg;lkd jklgds
+        klgjkfld kgll slklskgl flkjgl;s slkjsglk sklklg sklgjl slkjlg lksjg
+        slkfj{" "}
+      </p>
+    </div>
+  )
+}
+
+export default ServicesPage
